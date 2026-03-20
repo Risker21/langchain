63b3065948fe8fa1222f3118ef60f254dbb84bc0
Risker21 <3225298824@qq.com>
2026-03-20 11:49:33 +0800

    提交

---
+++ agent/01agent实现.py
@@ -0,0 +1,25 @@
+"""
+创建静态模型的agent
+
+"""
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langgraph.graph.state import CompiledStateGraph
+
+from models.tools.模型工具调用 import tool_call
+from my_llm import deepseek_llm
+
+
+@tool
+def get_SchoolMajor(school: str) -> str:
+    """获取指定学校的专业信息"""
+    return f"{school}的专业信息是：计算机科学、机械工程、电子工程等。"
+
+
+agent:CompiledStateGraph = create_agent(deepseek_llm,tools=[get_SchoolMajor])
+
+# print(type(agent))
+
+resp:dict = agent.invoke({"messages":[{"role":"user","content":"请告诉我清华大学的专业信息"}]})
+print(type(resp))
+print(resp)
